4533cb0fc056c674e4c8da735e41769f394360ef
Pavan <pavankalyan.Araveti1999@gmail.com>
2025-11-12 12:00:35 +0530

    Variables

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PERSON1=Ramesh
+PERSON2=Suresh
+
+
+echo "PERSON1:: Hi how are you PERSON2"
+echo "PERSON2:: Iam fine what about you PERSON1"
+echo "PERSON1:: Iam good, what are yuou studying PERSON2"
+echo "PERSON2:: Iam studying english, what about you PERSON1"
