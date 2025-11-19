df651adfc41701594df7cc78fc387137694cc518
Pavan <pavankalyan.Araveti1999@gmail.com>
2025-11-19 17:12:09 +0530

    Installation

--- 12-installation.sh
+++ 12-installation.sh
@@ -15,11 +15,27 @@ VALIDATE(){ #function receive inputs through args just like shell script
         echo "Installing $2 is success"
     fi    
 }
-dnf install mysql -y
-VALIDATE $? "MySQL"
 
-dnf install nginx -y
-VALIDATE $? "nginx"
+dnf list installed mysql
+if [ $? -ne 0 ]; then
+    dnf install mysql -y
+    VALIDATE $? "MySQL"
+else 
+    echo "Mysql Package is already installed"
+fi
 
-dnf install python3 -y
-VALIDATE $? "python3"
+dnf list installed nginx
+if [ $? -ne 0 ]; then
+    dnf install nginx -y
+    VALIDATE $? "nginx"
+else 
+    echo "nginx Package is already installed"
+fi
+
+dnf list installed python3
+if [ $? -ne 0 ]; then
+    dnf install python3 -y
+    VALIDATE $? "python3"
+else 
+    echo "python3 Package is already installed"
+fi
