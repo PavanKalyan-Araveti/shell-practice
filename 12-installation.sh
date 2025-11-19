09acaafb240902fb0d149e159123008ab91aae6a
Pavan <pavankalyan.Araveti1999@gmail.com>
2025-11-19 17:04:51 +0530

    Installation

--- 12-installation.sh
+++ 12-installation.sh
@@ -7,27 +7,19 @@ if [ $USERID -eq 0 ]; then
     exit -1  #failur exit code is other than 0
 fi
 
+VALIDATE(){ #function receive inputs through args just like shell script
+    if [ $1 -e 0 ]; then
+        echo "ERROR:: Installing $2 is failure"
+        exit -1
+    else 
+        echo "Installing $2 is success"
+    fi    
+}
 dnf install mysql -y
-
-if [ $? -e 0 ]; then
-    echo "ERROR:: Installing mysql is failure"
-    exit -1
-else 
-    echo "Installing mysql is success"
-fi
+VALIDATE $? "MySQL"
 
 dnf install nginx -y
-
-if [ $? -eq 0 ]; then
-    echo "ERROR:: Installation nginx is failure:
-else
-    echo "Installating nginx is success"
-fi
+VALIDATE $? "nginx"
 
 dnf install python3 -y
-
-if [ $? -eq 0 ]; then
-    echo "ERROR:: Installation python3 is failure:
-else
-    echo "Installating phtyon is success"
-fi
+VALIDATE $? "python3"
