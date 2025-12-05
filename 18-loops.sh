19ea611b0ca9343b2288a0d96759c1d547a688de
Pavan <pavankalyan.Araveti1999@gmail.com>
2025-12-05 07:47:21 +0530

    fixed loop package issue

--- 18-loops.sh
+++ 18-loops.sh
@@ -5,17 +5,18 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-USERID=$(id - u)
+USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$LOG_FILE.log"
-mkdir -p $LOGS_FOLDER
 echo "Scripting started executing at : $(date)"
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR: Please run this script with root privilage"
     exit -1
 fi
+
+mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
